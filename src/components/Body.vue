6729468eacd2200c8300f74593fab33942531410
tomrob1 <44196045+tomrob1@users.noreply.github.com>
2021-07-05 15:46:54 +0100

    PDF view

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container-fluid">
       <div class="row">
-        <div class="col-md" id="main-body">
+        <div class="col"></div>
+        <div class="col" id="main-body">
             <p class="h3"> About Me </p>
             Studying Advanced Computer Science MSc, currently working on my Dissertation project that aims to create a  
             <a href="https://www.researchgate.net/publication/344547065_A_Case_for_a_Currencyless_Economy_Based_on_Bartering_with_Smart_Contracts">bartering web application</a>
@@ -9,10 +10,11 @@
             <hr>
             I have experience in front-end and back-end development, having used languages such as Java, Python, JavaScript, PHP, MySQL, R and C in my studies and work experience.
         </div>
-        <div class="col-sm" id="links">
+        <div class="col" id="links">
         <p class="h4">Links</p>
         <b-icon icon="github"></b-icon><a href="https://github.com/tomrob1">Github</a> <br>
-        <b-icon icon="file-earmark-person"></b-icon><a href="">CV</a>
+        <b-icon icon="file-earmark-person"></b-icon><a :href=pdfLink target="_blank">CV</a> <br>
+        <b-icon icon="envelope"></b-icon><a href="mailto:tomrobertson599@hotmail.co.uk" target="_blank">Email</a>
         </div>
       </div>
   </div>
@@ -21,7 +23,10 @@
 <script>
 export default {
   name: 'Body',
-  props: {
+  data(){
+    return {
+      pdfLink : require('../assets/NEW_CV.pdf')
+    }
   }
 }
 </script>
@@ -29,7 +34,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #main-body{
-  margin-left: 100px;
   margin-top: 30px;
 }
 #links{
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
     devServer: {
       disableHostCheck: true
-    }
+    },
+    chainWebpack: config => {
+        config.module
+          .rule('vue')
+          .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+              // modify the options...
+              return options
+            })
+      
+        config.module.rule('pdf')
+          .test(/\.pdf$/)
+          .use('file-loader').loader('file-loader')
+    
+      }
   }
